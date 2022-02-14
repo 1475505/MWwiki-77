517bc9c3906ac5971bec6a4ce22f7e08001786c4
liuliang <liul2566@163.com>
2022-02-15 04:30:08 +0800

    3-3 增加数据库all.sql

--- src/main/java/com/liul2566/wiki/controller/TestController.java
+++ src/main/java/com/liul2566/wiki/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.liul2566.wiki.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
     }
 
+    @PostMapping("/hello/post")
+    public String hello(String name){
+        return "Hello-a " + name;
+    }
 }
