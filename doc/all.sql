3174abeda4073fb975ee984c9e3a33ae3b9ae1e7
liuliang <liul2566@163.com>
2022-02-15 16:49:34 +0800

    3-5集成mybatis自动生成代码 Demo

--- doc/all.sql
+++ doc/all.sql
@@ -1,10 +1,24 @@
-drop table if exists `test`;
-create table `test`
+drop table if exists `demo`;
+create table `demo`
 (
     `id`   bigint not null comment 'id',
     `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
+
+insert into `demo` (id, name)
+values (1, '测试');
+
+drop table if exists `test`;
+create table `test`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment '名称',
     `password` varchar(50) comment '密码',
     primary key (`id`)
-)engine=innoDB default charset=utf8mb4 comment='测试';
+) engine = innoDB
+  default charset = utf8mb4 comment ='测试';
 
-insert into `test` (id, name,password) values (2, '测试','测试');
+insert into `test` (id, name, password)
+values (1, '测试', '测试');
