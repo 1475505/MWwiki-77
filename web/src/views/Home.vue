d593936dc0931797cd7c3d44cd3ba97740421878
liuliang <liul2566@163.com>
2022-02-17 01:41:23 +0800

    4-6 首页布局开发

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
+    <div>
+      <a-button type="primary" danger>Primary</a-button>
+      <a-button danger>Default</a-button>
+      <a-button type="dashed" danger>Dashed</a-button>
+      <a-button type="text" danger>Text</a-button>
+      <a-button type="link" danger>Link</a-button>
+    </div>
+
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
