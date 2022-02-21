7a8e064ef3afa18793b09043c77e0737002dba30
liuliang <liul2566@163.com>
2022-02-21 20:04:44 +0800

    10-4 定时任务执行示例

--- src/main/java/com/liul2566/wiki/WikiApplication.java
+++ src/main/java/com/liul2566/wiki/WikiApplication.java
@@ -7,9 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan("com.liul2566")
 @MapperScan("com.liul2566.wiki.mapper")
+@EnableScheduling
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
